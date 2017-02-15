c6040709b7d88a0ea06778eccdbd05bf143c2d93
emilianocarvalho <emilianocarvalho.it@gmail.com>
2017-02-14 23:12:29 -0300

    Admim pessoa fisica, juridica e telemarketing

--- app/view/view-admin/pessoa-fisica.php
+++ app/view/view-admin/pessoa-fisica.php
@@ -1,39 +1,69 @@
-<?php include_once ('app/view/partlals/header.php') ?>
-<form class="form-inline" method="post" action="/?controller=produtos&action=search">
-  <div class="form-group">
-    <input type="txt" class="form-control" id="empresa" name="param" placeholder="Buscar por Id">
-  </div>
-  <button class="btn btn-primary">Buscar</button>
-</form>
-  <?php if(count($listaspf) > 0) {?>
-<table class="table table-striped table-bordered btn-primary">
-  <legend>Dados - Pessoa Fisica</legend>
-  <thead>
-    <tr>
-      <th>
-        <p>CPF</p>
-      </th>
-      <th>
-      <th>
-        <p>TELEFONE</p>
-      </th>
-      <th>
-      <th>
-        <p>DATA CADASTRO</p>
-      </th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach ($listaspf as $key) { ?>
-      <tr style="color:#000">
-          <td><?=$key['cpf'] ?></td>
-          <td></td>
-          <td><?=$key['telefone_numero'] ?></td>
-          <td></td>
-          <td><?=$key['data_cadastro'] ?></td>
-      </tr>
-    <?php } ?>
-  </tbody>
-  </table>
-<?php } ?>
-<?php include_once ('app/view/partlals/footer.php') ?>
+<?php include_once 'app/view/partlals/header.php' ?>
+<script src="app/assets/js/update-form.js" charset="utf-8"></script>
+<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.debug.js"></script>-->
+<section class="hero np-padding-20">
+    <div class="npTitle">
+        <h1 class="title is-4">
+            Administração
+        </h1>
+        <h2 class="subtitle is-6">
+            Pessoa Física
+        </h2>
+        <hr>
+
+        <div class="columns">
+            <div class="column">
+                <!-- <div class="control"> -->
+                <form class="control" method="post" action="/search">
+                    <input class="input-w-10" type="text" id="empresa" name="param" placeholder="Buscar por nome" >
+                    <input class="button is-primary is-outlined" type="submit" value="Buscar"/>
+                    <span class="help">Digite o nome para localizar.</span>
+                </form>
+                <!-- </div> -->
+            </div>
+        </div>
+        <div class="columns">
+            <div class="column">
+                <div class="panel">
+                    <div class="panel-heading">
+                        <p class="title is-6">
+                            <strong>Pessoas Físicas</strong>
+                        </p>
+                    </div>
+                    <div class="panel-block">
+                        <?php if(count($listaspf) > 0) {?>
+                            <table class="table table-striped table-narrow">
+                                <thead>
+                                    <tr>
+                                        <th>
+                                            <p>CPF</p>
+                                        </th>
+                                        <th>
+                                            <p>TELEFONE</p>
+                                        </th>
+                                        <th>
+                                            <p>DATA CADASTRO</p>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <form>
+                                        <?php foreach ($listaspf as $key) { ?>
+                                            <tr style="color:#000">
+                                                <td><?=$key['cpf'] ?></td>
+                                                <td><?=$key['telefone_numero'] ?></td>
+                                                <td><?=$key['data_cadastro'] ?></td>
+                                            </tr>
+                                        <?php } ?>
+                                    </form>
+                                </tbody>
+                            </table>
+                            <?php } ?>
+                            <p>&nbsp;</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <?php include_once ('app/view/partlals/footer.php') ?>
